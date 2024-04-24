fcb14c763f8f2c8512d412b2cac2130858802c2e
Dorian <doriangbg22@gmail.com>
2024-04-24 04:20:46 -0600

    Updates

--- js/nombre.js
+++ js/nombre.js
@@ -1,41 +1,11 @@
-alert("TEST")
-alert("Commit")
+import { initBackend } from 'absurd-sql/dist/indexeddb-main-thread';
 
-//const fs = require('fs');
-const sqlite3 = require('sqlite3').verbose();
+function init() {
+  let worker = new Worker(new URL('./index.worker.js', import.meta.url));
+  // This is only required because Safari doesn't support nested
+  // workers. This installs a handler that will proxy creating web
+  // workers through the main thread
+  initBackend(worker);
+}
 
-// Crear una nueva base de datos en memoria
-const db = new sqlite3.Database(':memory:');
-
-// Nombre del archivo SQLite en disco
-const filename = './db/osi.db';
-
-// Abrir una conexión a la base de datos en disco
-const dbOnDisk = new sqlite3.Database(filename);
-
-// Consulta para obtener todos los datos de una tabla
-const query = 'CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY,value TEXT)';
-
-// Ejecutar la consulta en la base de datos en disco y insertar los datos en la base de datos en memoria
-dbOnDisk.each(query, (err, row) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        // Insertar un valor de testeo en la tabla
-        const insertQuery = 'INSERT INTO test_table (value) VALUES (?)';
-        const testValue = 'Hola, mundo!';
-
-        db.run(insertQuery, [testValue], (err) => {
-            if (err) {
-                console.error('Error al insertar el valor de testeo:', err.message);
-            } else {
-                console.log('Valor de testeo insertado correctamente.');
-            }
-        });
-    }
-});
-
-// Cerrar la conexión a la base de datos en disco
-dbOnDisk.close();
-
-// Ahora la base de datos en memoria contiene los datos del archivo en disco
+init();
